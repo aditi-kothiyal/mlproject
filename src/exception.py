c521b17a7020fb59af63e2ab2e121cde8aa81f22
Aditi Kothiyal <aditikothiyal700@gmail.com>
2025-10-20 00:56:05 +0530

    Data Ingestion

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 
 def error_message_detail(error, error_detail: sys):
     _, _, exc_tb = error_detail.exc_info()
@@ -20,4 +20,5 @@ class CustomException(Exception):
 #     try:
 #         a = 1 / 0
 #     except Exception as e:
+#         logging.info("Divide by 0 error occurred")
 #         raise CustomException(e, sys)
